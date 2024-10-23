632553ade489f6512bdc949ddb340ba6620874fa
Hasan Saju <hasansaju11@gmail.com>
2024-10-23 19:38:24 -0400

    Client creation and bank account creation added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Bank-Teller-App
-A tool for bank employees to manage customer accounts and cards, conduct transactions, and detect fraud with machine learning support
+A tool for bank employees to manage customer accounts and cards, conduct transactions, and detect fraud with machine learning support.
+
+## Tech Stack
+- **Django**
+- **SQLite**
+- **Postman** - API Testing
+- **Machine Learning**:
+  - TensorFlow
+  - Keras
+  - PyTorch
--- server/BankTellerProject/AccountApp/views.py
+++ server/BankTellerProject/AccountApp/views.py
@@ -1,3 +1,106 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import random
+from .serializers import ClientSerializer, BankAccountSerializer
+from .models import Client, BankAccount
 # Create your views here.
+
+def account_list(request):
+    accounts = BankAccount.objects.all()
+    serializer = BankAccountSerializer(accounts, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+@csrf_exempt
+def create_newClient(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        occupation = data.get('occupation')
+        address = data.get('address')
+        email = data.get('email')
+        phone = data.get('phone')
+        nominee = data.get('nominee')
+
+        # Generate a unique 9-digit client ID
+        client_id = str(random.randint(100000000000, 999999999999))
+
+        
+        # Fetch all existing client IDs
+        existing_client_ids = set(Client.objects.values_list('client_id', flat=True))
+
+        # Generate a unique 12-digit client ID
+        while True:
+            client_id = str(random.randint(100000000000, 999999999999))
+            if client_id not in existing_client_ids:
+                break
+
+        # Create a new Client object
+        client = Client(
+            client_id=client_id,
+            firstname=firstname,
+            lastname=lastname,
+            occupation=occupation,
+            address=address,
+            email=email,
+            phone=phone,
+            nominee=nominee
+        )
+
+        # Save the Client object to the database
+        client.save()
+        response_data = {
+            'client_id': client_id,
+            'user_name': f"{firstname} {lastname}"
+        }
+
+        return JsonResponse(response_data, status=201)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+
+@csrf_exempt
+def create_newAccount(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        client = data.get('client')
+        account_type = data.get('account_type')
+        branch = data.get('branch')
+
+        # Check if the client exists
+        try:
+            client = Client.objects.get(client_id=client)
+        except Client.DoesNotExist:
+            return JsonResponse({'error': 'Client does not exist'}, status=404)
+
+        
+        # Fetch all existing client IDs
+        existing_account_ids = set(BankAccount.objects.values_list('account_no', flat=True))
+
+        # Generate a unique 12-digit client ID
+        while True:
+            account_no = str(random.randint(100000000, 999999999))
+            if account_no not in existing_account_ids:
+                break
+
+        # Create a new Client object
+        bankAccount = BankAccount(
+           account_no=account_no,
+            client=client,
+            account_type=account_type,
+            balance=0,
+            branch=branch
+        )
+
+        # Save the Client object to the database
+        bankAccount.save()
+        response_data = {
+            'account_no': account_no,
+            'nameOfAccount': f"{client.firstname} {client.lastname}"
+        }
+
+        return JsonResponse(response_data, status=201)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
