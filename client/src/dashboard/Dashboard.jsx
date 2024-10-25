641607faac5cb0fd8d41547dcb6897b3caf465b6
Maher Muhtadi <m.muhtadi2002@gmail.com>
2024-10-24 20:22:33 -0400

    made dashboard widgets more interactive, added agenda view

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -1,16 +1,15 @@
 import Clients from "./Clients";
 import Transactions from "./Transactions";
+import Agenda from "./Agenda";
 
 function Dashboard() {
     return (
-        <div className="flex justify-between min-h-screen p-4">
-            <div className="rounded-xl bg-neutral-50 w-1/4 h-1/1 p-4 shadow-md">
-                <h2 className="font-bold">Schedule</h2>
-            </div>
-            <div className="rounded-xl bg-neutral-50 w-2/5 h-1/1 p-4 shadow-md">
+        <div className="flex justify-between h-full p-4">
+            <Agenda />
+            <div className="rounded-xl bg-neutral-50 w-2/5 p-4 shadow-md">
                 <h2 className="font-bold">Activities</h2>
             </div>
-            <div className="flex flex-col space-y-6 w-1/4 h-1/1 pl-4">
+            <div className="flex flex-col space-y-6 w-1/4 pl-4">
                 <Clients />
                 <Transactions />
             </div>
